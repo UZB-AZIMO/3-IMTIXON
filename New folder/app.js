d2f3d1c7c079f3098c1e328c896204e22cb9d5dc
UZB-AZIMO <abduvorisazimov712@gmail.com>
2024-06-28 21:40:50 +0500

    imtixon

---
+++ New folder/app.js
@@ -0,0 +1,123 @@
+
+// {4-masala
+
+// function son(k,n){
+//     for(let i = 1; i <=n; i++){
+//         console.log(k);
+//     }
+//     return "natija tayyor"
+// }
+// console.log(son(5 ,6))
+
+// }
+
+
+    // { 6-masala
+
+//                 function hisob(a, b , amal){
+// let natija;
+//   switch (amal) {
+//     case 1:
+//       natija = a + b;
+//   }
+//   console.log(natija);
+//   return natija
+// }
+// hisob(3, 4);
+
+// function hisob(a, b){
+//     natija = a + b;
+    
+// }
+// console.log(natija);
+
+
+// } 
+
+// 7-masala
+  
+    // function foo(array){
+    //             let sum = []
+    //             for(let i = 0, len = array.length; i < len; i++){
+    //              if(array[i] > 0){
+    //                 sum.push(array[i])
+    //              }
+    //             }
+    //             return sum
+    //         }
+    //         console.log(foo([-5, 3, 5, -2]));
+
+// }
+    
+// {8-masala
+
+// function sum(a, b , amal){
+//     let natija;
+//       switch (amal) {
+//         case 1:
+//           natija = a * b;
+//       }
+//       console.log(natija);
+//       return natija
+//     }
+//     sum(3, 127000);
+    
+    
+//     function sum(a, b){
+//         natija = a * b;
+        
+//     }
+//     console.log 
+    
+//     console.log(natija);
+    
+    // }
+
+    
+  // {  9-masala
+
+    // function foo(array){
+    //             let sum = []
+    //             for(let i = 0, len = array.length; i < len; i++){
+    //              if(array[i] > 0){
+    //                 sum.push(array[i])
+    //              }
+    //             }
+    //             return sum
+    //         }
+    //         console.log(foo([-5, 3, 5, -2]));
+            // }
+
+// }
+  
+  
+
+//   // {10-masala
+
+// function dayName(n){ 
+//     switch(n){
+//         case 1:
+//             return "BU KUNI DUSHANBA"
+
+//              case 2:
+//             return "BU KUNI SESHANBA"
+
+//              case 3:
+//             return "BU KUNI UCHORSHANBA"
+
+//              case 4:
+//             return "BU KUNI PAYSHANBA"
+//              case 5:
+//             return "BU KUNI JUMA"
+//              case 6:
+//             return "BU KUNI SHANBA"
+
+//              case 7:
+//             return "BU KUNI YAKSHANBA"    
+// default:
+//     console.log("bunday kun yo'q")   
+//     }
+// }
+// console.log(dayName(8))
+
+// // }
